9cb10815a9a35d1124aaf0d9ef7c329dd927d633
Ashish Karn <wilsonwade.one8@gmail.com>
2021-04-25 22:00:14 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
+import { Container} from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import SimpleBottomNavigation from './Components/MainNav/MainNav';
+import Trending from './Components/Pages/Trending/Trending';
+import Movies from "./Components/Pages/Movies/Movies";
+import Search from './Components/Pages/Search/Search';
+import Series from './Components/Pages/Series/Series';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <div className="app">
+        <Container>
+        <Switch>
+          <Route path='/' component={Trending} exact></Route>
+          <Route path='/movies' component={Movies}></Route>
+          <Route path='/series' component={Series}></Route>
+          <Route path='/search' component={Search}></Route>
+        </Switch>
+        </Container>
+      </div>
+      
+      <SimpleBottomNavigation/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+// /home/spectre/the-movie-gallery/src
+
+
+const Header = () => {
+    return (
+        <span className="header">The Movie Gallery</span>
+ 
+    )
+};
+
+export default Header;
